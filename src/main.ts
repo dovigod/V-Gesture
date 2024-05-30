f22977ba9531c0c65757f5e3130c03aafebd82bf
dovigod <dovimccalister@gmail.com>
2024-05-30 18:29:05 +0900

    fix: disable pointer events on canvas

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 // import { VGesture } from '../build/vgesture'
 // import { ClickGesturePlugin } from '../build/plugins'
 // import { Handedness } from './types';
+import { initialize } from "fastdom/extensions/fastdom-promised";
 import { VGesture } from "./VGesture";
 import { ClickGesturePlugin } from "./plugins";
 async function init() {
@@ -21,22 +22,30 @@ async function init() {
   //   },
   // });
 
-  const vGesture = new VGesture()
+  let vGesture = new VGesture()
 
 
   await vGesture.initialize();
   const clickGesturePlugin = new ClickGesturePlugin({
   })
-
-
   vGesture.register(clickGesturePlugin)
   await vGesture.startDetection()
   // vGesture.endDetection()
 
+  const p = document.getElementById('toggle')!
 
-  setTimeout(() => {
-    const t = document.getElementById('btn1')
-    t!.style.left = '300px'
-  }, 3000)
+  let i = 0
+  p.addEventListener('click', async () => {
+    if (i % 2 === 0) {
+      vGesture.endDetection()
+    } else {
+      vGesture = new VGesture();
+      await vGesture.initialize();
+      vGesture.register(clickGesturePlugin)
+      vGesture.startDetection()
+    }
+
+    i++;
+  })
 }
 init()
